75a5e053b044be020d3c1425c59a5bf94f619231
Bojan Petrovic <bojan@embroker.com>
2016-02-18 18:21:59 +0100

    Add "test" cmd line flag

--- codeshiptest.go
+++ codeshiptest.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/bojan-embroker/codeshiptest/dump"
 )
 
 func main() {
+
+	test := flag.String("test", "", "Blah")
+	flag.Parse()
+
+	if *test == "success" {
+		log.Println("Success")
+		os.Exit(0)
+	}
+
+	if *test == "fail" {
+		log.Println("Fail")
+		os.Exit(-1)
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
